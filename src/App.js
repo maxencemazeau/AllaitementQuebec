a305998d9c61cf73d1be7cbf81e298aaacc38b9f
florian hostachy <2032399@cegeprdl.ca>
2023-01-30 15:20:42 -0500

    1.0
    Mise en route

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Accueil from './pages/accueil';
+
+function App() {
+  return (
+    <div className="App">
+      <Accueil/>
+    </div>
+  );
+}
+
+export default App;
