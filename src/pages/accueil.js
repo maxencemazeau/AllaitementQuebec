5bec5d27d048859b0c91978f84e89e7387022851
maxence <maxencemazeau@outlook.fr>
2023-02-01 08:05:50 -0500

    accueil

--- src/pages/accueil.js
+++ src/pages/accueil.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
+import MetaTags from 'react-meta-tags';
 
 export default function accueil() {
   return (
     <React.Fragment>
-            
+        <Helmet>
+        
+            <MetaTags>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, shrink-to-fit=no, viewport-fit=cover" />
+                <meta name="color-scheme" content="light dark" />
+            </MetaTags>
+
+        
+
+       
+        <link rel="shortcut icon" href="./assets/img/favicon/favicon.ico" type="image/x-icon" />
+
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700" rel="stylesheet" />
+
+        
+        <link class="css-lt" rel="stylesheet" href="./assets/css/template.bundle.css" media="(prefers-color-scheme: light)" />
+        <link class="css-dk" rel="stylesheet" href="./assets/css/template.dark.bundle.css" media="(prefers-color-scheme: dark)" />
+
+        </Helmet>
 
         <div className="layout overflow-hidden">
             
