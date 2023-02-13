41bb697a601499c4c91a562e9a701029e711af4a
florian hostachy <2032399@cegeprdl.ca>
2023-02-13 09:38:49 -0500

    1,1
    Commit avec le code corrigé a 100% et nav bar a 90%

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react'
 
+
+
 export class Header extends Component {
   render() {
     return (
         <nav className="navigation d-flex flex-column text-center navbar navbar-light hide-scrollbar">
-      
-          <a href="index.html" title="Messenger" className="d-none d-xl-block mb-6">
-              <svg version="1.1" width="46px" height="46px" fill="currentColor" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 46 46" enableBackground="new 0 0 46 46" xmlSpace="preserve">
-                  <polygon opacity="0.7" points="45,11 36,11 35.5,1 "/>
-                  <polygon points="35.5,1 25.4,14.1 39,21 "/>
-                  <polygon opacity="0.4" points="17,9.8 39,21 17,26 "/>
-                  <polygon opacity="0.7" points="2,12 17,26 17,9.8 "/>
-                  <polygon opacity="0.7" points="17,26 39,21 28,36 "/>
-                  <polygon points="28,36 4.5,44 17,26 "/>
-                  <polygon points="17,26 1,26 10.8,20.1 "/>
-              </svg>
-    
-          </a>
-    
-    
+ 
           <ul className="d-flex nav navbar-nav flex-row flex-xl-column flex-grow-1 justify-content-between justify-content-xl-center align-items-center w-100 py-4 py-lg-2 px-lg-3" role="tablist">
           
               <li className="nav-item d-none d-xl-block invisible flex-xl-grow-1">
