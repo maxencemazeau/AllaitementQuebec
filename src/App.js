119843c4feee5d362a1b36ba2e049a95c94df701
thaninaadda <83210394+thaninaadda@users.noreply.github.com>
2023-03-04 10:04:09 -0500

    modification dans app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,36 @@ import SideBar from './components/SideBar';
 import { Body } from './components/Body';
 import { FaHome } from "react-icons/fa";
 import Dropdown from 'react-bootstrap/Dropdown';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import axios from "axios";
+
 
 
 import InscriptionParent from '../src/components/Parent/Inscription/Inscription';
 import ConnexionUtilisateur from '../src/components/Connexion/Connexion';
 import InscriptionBenevole from '../src/components/Benevole/Inscription/Inscription';
 import CreationActivite from '../src/components/Activite/CreationActivite/Creation';
-
+import CreerChat from './components/ChatParent/CreerChat';
+import Chat from './components/ChatParent/Chat';
+import Connexion from '../src/components/Connexion/Connexion';
+import TableauDeBord from './components/Parent/Inscription/TableauDeBord';
+ 
 function App() {
+
+  const id = 66;
+
+  const Deconnexion = () => {
+    axios.put(`http://localhost:8080/chatFini/${id}`);
+    sessionStorage.clear();
+    window.location.href = '/Connexion';
+    console.log("Session clear");
+  }
+  
   return (
     <div className="App">
       <nav class="navbar navbar-expand-lg navbar-light  " style={{ backgroundColor: 'rgba(61, 72, 139, 1)', padding: "1.2%" }}>
@@ -72,7 +94,28 @@ function App() {
         {/* <Header/>
         <SideBar/>
         <Body/> */}
-        <ConnexionUtilisateur></ConnexionUtilisateur>
+         <Router>
+      <div>
+        <nav>
+          <ul>
+          
+          <li><Link to="/Connexion">Connexion</Link></li>
+          <button onClick={Deconnexion}>Deconnexion</button>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/Chat" element={<Chat />}></Route>
+          <Route path="/Connexion" element={<Connexion />}></Route>  
+          <Route path="/CreerChat" element={<CreerChat />}></Route>
+          <Route path="/TableauDeBord" element={<TableauDeBord />}></Route>
+        </Routes>
+         
+      </div>
+    </Router>
+        
       </div>
     </div>
   );
